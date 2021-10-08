959c1d0d768df58c21baa3daa30add826d7cd7c5
maximbalyk <maximbalyk@gmail.com>
2021-10-08 12:12:07 +0300

    deploy

---
+++ precache-manifest.3c80d3f1afdcd3d9396d690875440ed5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ec841feb5e23caf5319",
+    "url": "/VueShop-test/css/app.766e85a0.css"
+  },
+  {
+    "revision": "9ec841feb5e23caf5319",
+    "url": "/VueShop-test/js/app.6cd04348.js"
+  },
+  {
+    "revision": "cbcf4cd837b9e0d79a9f",
+    "url": "/VueShop-test/css/chunk-2402fe05.92647ec0.css"
+  },
+  {
+    "revision": "cbcf4cd837b9e0d79a9f",
+    "url": "/VueShop-test/js/chunk-2402fe05.95cfa911.js"
+  },
+  {
+    "revision": "e5631bec245e8a55022f",
+    "url": "/VueShop-test/js/chunk-2d2086b7.ea7d9901.js"
+  },
+  {
+    "revision": "f7210eaac525abb9b03c",
+    "url": "/VueShop-test/js/chunk-2d217357.af49cba0.js"
+  },
+  {
+    "revision": "f46e142f173ecf1b3096",
+    "url": "/VueShop-test/js/chunk-2d217a99.c0bd02f8.js"
+  },
+  {
+    "revision": "19e9c4050266f3bdee54",
+    "url": "/VueShop-test/js/chunk-3828022c.7d3ddf14.js"
+  },
+  {
+    "revision": "128159e34882607d55db",
+    "url": "/VueShop-test/css/chunk-4e078908.92647ec0.css"
+  },
+  {
+    "revision": "128159e34882607d55db",
+    "url": "/VueShop-test/js/chunk-4e078908.0fa379f0.js"
+  },
+  {
+    "revision": "004c11bc98d279ddbc97",
+    "url": "/VueShop-test/css/chunk-64f74e5e.92647ec0.css"
+  },
+  {
+    "revision": "004c11bc98d279ddbc97",
+    "url": "/VueShop-test/js/chunk-64f74e5e.07d64e04.js"
+  },
+  {
+    "revision": "da29e76c53a5a9583718",
+    "url": "/VueShop-test/css/chunk-803248e2.92647ec0.css"
+  },
+  {
+    "revision": "da29e76c53a5a9583718",
+    "url": "/VueShop-test/js/chunk-803248e2.bb8c78ca.js"
+  },
+  {
+    "revision": "32d39ea765db0d7193b4",
+    "url": "/VueShop-test/css/chunk-abbf885a.92647ec0.css"
+  },
+  {
+    "revision": "32d39ea765db0d7193b4",
+    "url": "/VueShop-test/js/chunk-abbf885a.77a9ecf0.js"
+  },
+  {
+    "revision": "73c3cb54bc62646617b8",
+    "url": "/VueShop-test/css/chunk-vendors.f1ff6d04.css"
+  },
+  {
+    "revision": "73c3cb54bc62646617b8",
+    "url": "/VueShop-test/js/chunk-vendors.75c396e5.js"
+  },
+  {
+    "revision": "19693f849af0737de3f694edcbbe01ed",
+    "url": "/VueShop-test/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/VueShop-test/robots.txt"
+  }
+];
